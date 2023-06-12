f7f90b0c51b7f71336f78b5767b89ba607098d0c
DevTimDub <136389883+DevTimDub@users.noreply.github.com>
2023-06-13 00:00:59 +0200

    Update loadit.js

--- loadit.js
+++ loadit.js
@@ -1,7 +1,4 @@
-const url = 'https://timetable-lookup.p.rapidapi.com/TimeTable/ARN/STN/20230620/';
-const params = new URLSearchParams({ Airline: 'FR' });
-
-fetch(url + '?' + params, {
+fetch('https://timetable-lookup.p.rapidapi.com/TimeTable/ARN/STN/20230620/?Airline=FR', {
   headers: {
     'X-RapidAPI-Key': 'f97e3dbab8mshb83b4a869cc05bbp1448fajsna77ddd7b5e76',
     'X-RapidAPI-Host': 'timetable-lookup.p.rapidapi.com'
